38e8dfc889aded3788356070304dc638043196d4
Kevin Kirsche <kevin.kirsche@verizon.com>
2017-02-17 14:31:03 -0500

    Extend syslog / logging output

--- cmd/poll.go
+++ cmd/poll.go
@@ -14,19 +14,147 @@
 
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"net"
+	"strings"
 
-// pollCmd represents the poll command
+	"github.com/Sirupsen/logrus"
+	"github.com/k-sone/snmpgo"
+	"github.com/spf13/cobra"
+	"ni.vzbi.com/stash/scm/ncsddos/inquirer2/libinquirer"
+)
+
+// pollCmd represents the minute command
 var pollCmd = &cobra.Command{
 	Use:   "poll",
-	Short: "Poll is used for time based polling of SNMP OIDs",
-	Long: `Poll is used when doing either cron or custom time based SNMP polling of
-an external IP addresses for data.`,
+	Short: "SNMP polling for use via cron",
+	Long: `Minute is used for per minute polling, most commonly via cron or
+another automated service. This does not automate the timing, a tool like cron
+must be used to loop this every minute.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
+		conf, err := libinquirer.ParseConfigFile(cfgFile)
+		if err != nil {
+			log.WithError(err).Errorln("Failed to parse configuration file")
+		}
+		log.WithField("requested_poll_qty", len(conf.Poll)).Infof("%s poll configurations provided", cfgFile)
+		for i, cfg := range conf.Poll {
+			logrus.WithField("iteration", i).Debugln("Beginning poll process")
+			logrus.WithField("version", cfg.Version).Debugln("SNMP version requested --- Validating")
+			sv := libinquirer.NewVersion(cfg.Version)
+
+			var auth *libinquirer.SNMPAuth
+			if sv.Get() == "v3" {
+				auth, err = libinquirer.NewAuth(cfg.Username, cfg.SecurityLevel, cfg.AuthPassword, cfg.AuthProtocol, cfg.PrivPassword, cfg.PrivProtocol)
+				if err != nil {
+					log.WithError(err).Errorln("Failed to create SNMP V3 authentication object")
+					return
+				}
+			}
+
+			logrus.WithField("version", cfg.Version).Debugln("SNMP version accepted")
+
+			logrus.WithField("host", cfg.Host).Debugln("Looking up host's DNS name")
+			dnsName, err := net.LookupAddr(cfg.Host)
+			if err != nil {
+				logrus.WithError(err).Errorln("Failed to resolve IP Address to DNS name")
+			}
+
+			logrus.WithFields(logrus.Fields{
+				"host":             cfg.Host,
+				"dns_name":         strings.Join(dnsName, "|"),
+				"community_string": cfg.Community,
+				"retries":          cfg.Retries,
+				"version":          cfg.Version,
+			}).Debugln("Creating SNMP client")
+			client, err := libinquirer.CreateClient(cfg.Host, cfg.Community, cfg.Retries, sv, auth)
+			if err != nil {
+				log.WithError(err).Errorln("Failed to create SNMP client")
+				return
+			}
+			logrus.Debugln("SNMP client created")
+
+			logrus.Debugln("Generating OID object for querying process")
+			stroids := []string{}
+			for oid, mib := range cfg.OIDs {
+				logrus.WithFields(logrus.Fields{
+					"oid": oid,
+					"mib": mib,
+				}).Debugln("Adding object for querying")
+				stroids = append(stroids, oid)
+			}
+
+			oids, err := snmpgo.NewOids(stroids)
+			if err != nil {
+				log.WithError(err).Errorln("Failed to create OIDs object")
+				continue
+			}
+			logrus.Debugln("OID object successfully created")
+
+			logrus.WithFields(logrus.Fields{
+				"host":     cfg.Host,
+				"dns_name": strings.Join(dnsName, "|"),
+			}).Debugln("Creating client connection to host")
+			err = client.Open()
+			if err != nil {
+				log.WithError(err).Errorln("Failed to open SNMP connection")
+				continue
+			}
+			logrus.Debugln("Client connection created successfully")
+
+			logrus.WithFields(logrus.Fields{
+				"nonrepeaters":   0,
+				"maxrepetitions": 10,
+			}).Debugln("Beginning bulk walk")
+			pdu, err := client.GetBulkWalk(oids, 0, 10)
+			if err != nil {
+				log.WithError(err).Errorln("Failed to execute bulk walk request")
+				client.Close()
+				continue
+			}
+			logrus.Debugln("PDU's retrieved, checking for PDU error(s)")
+			if pdu.ErrorStatus() != snmpgo.NoError {
+				log.WithFields(logrus.Fields{
+					"error_index": pdu.ErrorIndex(),
+				}).Errorln(pdu.ErrorStatus())
+				client.Close()
+				continue
+			}
+			logrus.Debugln("No PDU errors found")
+
+			logrus.Debugln("Bulk walk completed successfully")
+			logrus.Debugln("Outputting result values")
+			for _, val := range pdu.VarBinds() {
+				splitOID := strings.Split(val.Oid.String(), ".")
+				oid := strings.Join(splitOID[:len(splitOID)-1], ".")
+				intIndex := strings.Join(splitOID[len(splitOID)-1:len(splitOID)], ".")
+				log.WithFields(logrus.Fields{
+					"full_oid":        val.Oid,
+					"host_queried":    cfg.Host,
+					"dns_name":        strings.Join(dnsName, "|^|"),
+					"oid":             oid,
+					"oid_name":        cfg.OIDs[fmt.Sprintf(".%s", oid)],
+					"interface_index": intIndex,
+					"type":            val.Variable.Type(),
+					"value":           val.Variable,
+				}).Infoln("OID successfully retrieved")
+			}
+			logrus.Debugln("Host output complete")
+			client.Close()
+		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(pollCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// pollCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// pollCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
